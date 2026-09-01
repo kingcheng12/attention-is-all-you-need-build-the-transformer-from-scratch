74e6c7341cc93c7b294d629f6e4c5e643cd36de3
Cat2333 <gongjinghao.cheng@gmail.com>
2026-09-01 18:16:06 -0400

    Step 36: normalize_and_scale_with_gamma_beta

--- model.py
+++ model.py
@@ -364,8 +364,15 @@ def compute_layer_norm_mean_and_variance(x):
 
     return mean, variance
 
-# Step 36 - normalize_and_scale_with_gamma_beta (not yet solved)
-# TODO: implement
+# Step 36 - normalize_and_scale_with_gamma_beta
+import torch
+
+def normalize_and_scale_with_gamma_beta(x, gamma, beta, eps=1e-5):
+    # TODO: standardize x along the last axis then apply gamma and beta affine transform
+    mean, variance = compute_layer_norm_mean_and_variance(x)
+    normalized = (x - mean) / torch.sqrt(variance + eps)
+
+    return gamma * normalized + beta
 
 # Step 37 - apply_residual_add_and_norm (not yet solved)
 # TODO: implement
